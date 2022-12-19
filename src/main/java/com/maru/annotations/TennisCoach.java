fb36782c3dc06d56dad700a31653aa47fa760fd3
marusosasssi <soassi@outlook.com>
2022-12-19 14:20:34 -0600

    creating setter methods in TennisCoach for injections and configuring the DI using Autowired

--- src/main/java/com/maru/annotations/TennisCoach.java
+++ src/main/java/com/maru/annotations/TennisCoach.java
@@ -8,12 +8,26 @@ public class TennisCoach implements Coach{
 
     private FortuneService fortuneService;
 
-    @Autowired //2. Configuring the dependency injection with @Autowired annotation
+    //using constructor injection:
+    /*@Autowired //2. Configuring the dependency injection with @Autowired annotation
     //1. creating constructor
     public TennisCoach(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }
     //spring will sacan for a component that implements the FortuneService interface, in our case is the HappyFortune
+    */
+
+    //default constructor:
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor.");
+    }
+
+    //defining setter method for injections
+    @Autowired //we tell spring to autowire this dependency
+    public void setFortuneService(FortuneService fortuneService){
+        System.out.println(">> TennisCoach: inside setFortuneService() method");
+        this.fortuneService = fortuneService;
+    }
 
     @Override
     public String getDailyWorkout() {
